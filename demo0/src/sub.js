6dfeedb261860029c8411871c386f4b0c79d6363
hubery <Hubery.Lee@outlook.com>
2016-03-25 16:02:04 +0800

    a webpack demo using CommonJs Style
    这个demo参照了http://zhuanlan.zhihu.com/FrontendMagazine/20367175

---
+++ demo0/src/sub.js
@@ -0,0 +1,8 @@
+//这里使用CommonJS的风格
+function generateText() {
+  var element = document.createElement('h4');
+  element.innerHTML = "Hello h4 world";
+  return element;
+}
+
+module.exports = generateText;
